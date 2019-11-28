335b5c99b05ada958b690781a5e2a779a5dc0c87
Nosferatu721 <elkintorres721@gmail.com>
2019-11-28 18:54:31 -0500

    Autoload Error Url

--- autoload.php
+++ autoload.php
@@ -2,6 +2,10 @@
 
 function autoloadController($className)
 {
+  $r = file_exists('controllers/' . $className . '.php');
+  if (!$r) {
+    header('Location: ' . baseUrl . 'error/index');
+  }
   require_once 'controllers/' . $className . '.php';
 }
 
